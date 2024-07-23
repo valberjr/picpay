b5817155cc6b644b729f4be795417811be437194
valberjr <valber@duck.com>
2024-07-23 11:28:58 -0300

    feat: mapped Transfer entity

---
+++ src/main/java/com/example/picpay/entity/Transfer.java
@@ -0,0 +1,58 @@
+package com.example.picpay.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_transfer")
+public class Transfer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "wallet_sender_id")
+    private Wallet sender;
+
+    @ManyToOne
+    @JoinColumn(name = "wallet_receiver_id")
+    private Wallet receiver;
+
+    @Column(name = "value")
+    private BigDecimal value;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Wallet getSender() {
+        return sender;
+    }
+
+    public void setSender(Wallet sender) {
+        this.sender = sender;
+    }
+
+    public Wallet getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(Wallet receiver) {
+        this.receiver = receiver;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+}
